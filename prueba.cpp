bca6a9da10a2b14ebcbd1ec51113814938cb1337
asdadasdasdasd asdasdasdasdasd <predator1ooooo@outlook.com>
2023-05-15 23:03:43 -0600

    Version 2

---
+++ prueba.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float d = 150;
+    float t = 3;
+    float velocidad = d/ t;
+
+    cout <<"hhhhh\n";
+
+    if(velocidad<100){
+        cout<< "ggggg";
+    }
+    
+}
---
+++ prueba2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    /*int n=5;
+    cout << n;
+    cout<< n++;
+    cout<< n;
+    n=5;
+    cout << n;
+    cout << ++n;
+    cout<< n;
+    return 0; */
+
+    int i = 0;
+
+    while(i<3){
+        i--;
+     cout << i;
+
+     
+
+    }
+
+
+
+}
